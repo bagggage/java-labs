a558eee34ffda6f4d94e493f06d2b5dbf3c4714b
bagggage <pigulevskiykostya@gmail.com>
2024-02-19 10:29:41 +0300

    Refactor

--- lab-1/src/main/java/com/example/lab/controller/UserController.java
+++ lab-1/src/main/java/com/example/lab/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.lab.model.User;
 import com.example.lab.service.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
