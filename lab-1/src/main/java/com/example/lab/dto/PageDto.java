79067fb0f584af4af94987a0b77ed0bcb7f440e8
bagggage <pigulevskiykostya@gmail.com>
2024-03-24 20:35:53 +0300

    Implemented pagenable search queries for git repositories

---
+++ lab-1/src/main/java/com/example/lab/dto/PageDto.java
@@ -0,0 +1,23 @@
+package com.example.lab.dto;
+
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+
+import lombok.Data;
+
+@Data
+public class PageDto<T> {
+    private List<T> content;
+
+    private int size;
+    private long number;
+    private long totalPages;
+
+    public PageDto (List<T> content, Pageable pageable, long totalPages) {
+        this.content = content;
+        this.size = pageable.getPageSize();
+        this.number = pageable.getPageNumber();
+        this.totalPages = totalPages;
+    }
+}
--- lab-1/src/main/java/com/example/lab/repository/GitRepository.java
+++ lab-1/src/main/java/com/example/lab/repository/GitRepository.java
@@ -1,11 +1,22 @@
 package com.example.lab.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.lab.entity.Git;
 
 public interface GitRepository extends JpaRepository<Git, Long> {
-    Optional<Git> findByName(String name);
+    List<Git> findAllByOwnerUsername(String username);
+    Optional<Git> findByNameAndOwnerUsername(String name, String username);
+
+    @Query(value = "SELECT g FROM Git g WHERE g.name iLIKE ?1")
+    Page<Git> searchByName(String name, Pageable pageable);
+
+    @Query(value = "SELECT g FROM Git g OUTER JOIN g.contributors c WHERE g.name iLIKE ?1 AND (g.owner.username = ?2 OR c.username = ?2)")
+    Page<Git> searchByNameAndUser(String name, String username, Pageable pageable);
 }
