b2ea54814762c82defd8e5ff1fefbb542f001977
bagggage <pigulevskiykostya@gmail.com>
2024-03-24 20:32:34 +0300

    Implemented cache

---
+++ lab-1/src/main/java/com/example/lab/cache/Cache.java
@@ -0,0 +1,81 @@
+package com.example.lab.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lombok.Getter;
+
+public class Cache<K,V> {
+    public interface ValueAccessor<T> {
+        T access();
+    }
+
+    public class CacheEntry {
+        private K key;
+        
+        public CacheEntry(Cache<K,V> cache, K key) {
+            this.key = key;
+        }
+
+        public V orDefault(V defaultValue) {
+            return content.getOrDefault(key, defaultValue);
+        }
+
+        public V orPut(ValueAccessor<V> valueAccessor) {
+            if (isCached(key)) return content.get(key);
+
+            V value = valueAccessor.access();
+
+            if (content.size() >= capacity) {
+                content.remove(content.keySet().iterator().next());
+            }
+
+            content.put(key, value);
+
+            return value;
+        }
+    }
+
+    @Getter
+    private Map<K,V> content;
+
+    @Getter
+    private int capacity;
+
+    public Cache(int maxCacheSize) {
+        content = new HashMap<>(maxCacheSize);
+        capacity = maxCacheSize;
+    }
+
+    public int size() {
+        return content.size();
+    }
+
+    public void clear() {
+        content.clear();
+    }
+
+    public boolean put(K key, V value) {
+        if (!content.containsKey(key)) return false;
+
+        content.put(key, value);
+
+        return true;
+    }
+
+    public boolean uncache(K key) {
+        if (!content.containsKey(key)) return false;
+
+        content.remove(key);
+
+        return true;
+    }
+
+    public boolean isCached(K key) {
+        return content.containsKey(key);
+    }
+
+    public CacheEntry tryGet(K key) {
+        return new CacheEntry(this, key);
+    }
+}
--- lab-1/src/main/java/com/example/lab/controller/GitController.java
+++ lab-1/src/main/java/com/example/lab/controller/GitController.java
@@ -3,15 +3,19 @@ package com.example.lab.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.example.lab.dto.GitDto;
+import com.example.lab.dto.PageDto;
 import com.example.lab.dto.UserDto;
 import com.example.lab.entity.Git;
 import com.example.lab.entity.User;
+import com.example.lab.exceptions.NotFoundException;
 import com.example.lab.service.GitService;
 
-import org.springframework.http.HttpStatus;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,19 +29,64 @@ public class GitController {
         this.service = service;
     }
 
-    @GetMapping
-    public GitDto getGitByName(@RequestParam(name = "name") String name) {
-        Git git = service.findGitByName(name).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    @GetMapping("/{username}")
+    public List<GitDto> getGitByOwner(@PathVariable(name = "username") String username) {
+        List<Git> gits = service.findGitsByOwnerUsername(username);
 
-        return new GitDto(git, true);
+        if (gits.isEmpty()) throw new NotFoundException();
+
+        List<GitDto> result = new ArrayList<>(gits.size());
+
+        for (Git git : gits) {
+            result.add(new GitDto(git, false));
+        }
+
+        return result;
     }
 
-    @PatchMapping("/{name}/contribute")
-    public UserDto addContributor(@PathVariable(name = "name") String name, @RequestParam(name = "username") String username) {
-        User contributor = service.addContributorByRepositoryName(name, username);
+    @GetMapping("/{username}/{name}")
+    public GitDto getGitByOwnerAndName(@PathVariable(name = "username") String username,
+                                    @PathVariable(name = "name") String name) {
+        Git git = service.findGitByNameAndUsername(name, username).orElseThrow(() ->
+                                                new NotFoundException());
+
+        return new GitDto(git, false);
+    }
+
+    @GetMapping("/search")
+    public PageDto<GitDto> searchGitsByNameAndUser(@RequestParam(name = "name") String name,
+                                                @RequestParam(name = "user", required = false) String username,
+                                                @RequestParam(name = "p", required = false) Integer pageNumber) {
+        Page<Git> page;
+
+        if (username == null) {
+            page = service.searchGitsByName(name, (pageNumber == null ? 0 : pageNumber));
+        }
+        else {
+            page = service.searchGitsByNameAndUser(name, username, (pageNumber == null ? 0 : pageNumber));
+        }
+
+        if (page.isEmpty()) throw new NotFoundException();
+
+        List<GitDto> dtoList = new ArrayList<GitDto>(page.getSize());
+
+        for (Git git : page) {
+            dtoList.add(new GitDto(git, true));
+        }
+
+        PageDto<GitDto> dtoPage = new PageDto<>(dtoList, page.getPageable(), page.getTotalPages());
+
+        return dtoPage;
+    }
+
+    @PatchMapping("/{username}/{name}/contribute")
+    public UserDto addContributor(@PathVariable(name = "username") String username,
+                                @PathVariable(name = "name") String name,
+                                @RequestParam(name = "contributor") String contributorUsername) {
+        User contributor = service.addContributorByRepositoryName(name, username, contributorUsername);
 
         if (contributor == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new NotFoundException();
         }
 
         return new UserDto(contributor, false);
